206e5539ed17ba2b22615561665fe17ed733bb03
Hampus <16954508+xdHampus@users.noreply.github.com>
2020-11-04 18:43:13 +0100

    Spiller klasse skabt ud fra UML.
    Skabte i sammenhæng med dette Action, Inventory og Sprite klassen.
    Disse klasser har dog intet indhold.

---
+++ src/worldofzuul/Inventory.java
@@ -0,0 +1,4 @@
+package worldofzuul;
+
+public class Inventory {
+}
---
+++ src/worldofzuul/Player.java
@@ -0,0 +1,16 @@
+package worldofzuul;
+
+import java.awt.geom.Point2D;
+
+public class Player {
+    public Point2D pos;
+    public Double balance;
+    public Inventory inventory;
+    public float Velocity;
+    public Action[] actions;
+    public Sprite sprite;
+
+    public Double getBalance() {
+        return balance;
+    }
+}
