e03b130be10590e2bacdecdae78893a97d1a295f
Hampus <16954508+xdHampus@users.noreply.github.com>
2020-11-05 16:53:26 +0100

    Implemented Plant.java

--- src/worldofzuul/Plant.java
+++ src/worldofzuul/Plant.java
@@ -1,4 +1,56 @@
 package worldofzuul;
 
-public class Plant {
+public class Plant extends Item {
+
+    private GrowthStage state = GrowthStage.SEED;
+    private float seedQuality;
+    private float waterNeeded;
+    private float nutritionNeeded;
+    private Disease disease;
+    private Pest pest;
+
+    public Plant(String name) {
+        super(name);
+    }
+    public Plant(String name, float seedQuality, float waterNeeded, float nutritionNeeded) {
+        super(name);
+        this.seedQuality = seedQuality;
+        this.waterNeeded = waterNeeded;
+        this.nutritionNeeded = nutritionNeeded;
+    }
+
+    public GrowthStage getState() {
+        return state;
+    }
+    public void setState(GrowthStage state) {
+        this.state = state;
+    }
+
+    public float getSeedQuality() {
+        return seedQuality;
+    }
+
+    public float getWaterNeeded() {
+        return waterNeeded;
+    }
+
+    public float getNutritionNeeded() {
+        return nutritionNeeded;
+    }
+
+    public Disease getDisease() {
+        return disease;
+    }
+
+    public void setDisease(Disease disease) {
+        this.disease = disease;
+    }
+
+    public Pest getPest() {
+        return pest;
+    }
+
+    public void setPest(Pest pest) {
+        this.pest = pest;
+    }
 }
