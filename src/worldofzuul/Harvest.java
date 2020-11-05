4d256f5381b64d988bbf5e494cd3be23f87d4d6c
Sune Roed <71824299+suroe20@users.noreply.github.com>
2020-11-05 18:31:34 +0100

    Ændring i Harvest og UML

--- src/worldofzuul/Harvest.java
+++ src/worldofzuul/Harvest.java
@@ -3,12 +3,15 @@ package worldofzuul;
 import java.util.Objects;
 
 public class Harvest extends Action {
+    double harvestAmount;
 
-    public Harvest(GameObjects[] items){
-        items = this.items;
+    public Harvest(double harvestAmount){
+        harvestAmount = this.harvestAmount;
     }
     @Override
     public Objects[] getItem(){
-        return this.items;
+        return this.Item;
     }
+    public double getHarvest(){
+        return this.harvestAmount;
 }
