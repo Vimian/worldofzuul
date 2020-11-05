03505d711799bd9851968760e3ed24a0db0fb87b
Sune Roed <71824299+suroe20@users.noreply.github.com>
2020-11-05 17:26:57 +0100

    Action ændret til en abstract klasse ud fra UML

---
+++ src/worldofzuul/Action.java
@@ -0,0 +1,14 @@
+import java.util.Objects;
+
+public abstract class Action {
+    public float efficiency;
+    public Sprite sprite;
+
+    public Action(float efficiency, Sprite sprite){
+        efficiency = this.efficiency;
+        sprite = this.sprite;
+    }
+    public Objects[] getItems(){
+        return items;
+    }
+}
