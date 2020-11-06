25256adaf1c1347edba49fa63aaf1fc0adcd39c3
Hampus <16954508+xdHampus@users.noreply.github.com>
2020-11-06 22:04:00 +0100

    Tilføjede felter til Inventory

--- src/worldofzuul/Inventory.java
+++ src/worldofzuul/Inventory.java
@@ -1,4 +1,21 @@
 package worldofzuul;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Inventory {
+    private LinkedList<Item> items = new LinkedList<>();
+
+    public Item getSelectedItem() {
+        return items.getFirst();
+    }
+    public void setSelectedItem(Item item) {
+        Collections.swap(items, items.indexOf(getSelectedItem()), items.indexOf(item));
+    }
+    public LinkedList<Item> getItems() {
+        return items;
+    }
+    public void setItems(LinkedList<Item> items) {
+        this.items = items;
+    }
 }
