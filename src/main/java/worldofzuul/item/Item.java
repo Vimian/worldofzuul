fc409d719625e92880c470bc720c15b2b32cda79
Sune <suneroed@gmail.com>
2020-11-17 16:32:06 +0100

    Structur Item rettet

--- src/main/java/worldofzuul/item/Item.java
+++ src/main/java/worldofzuul/item/Item.java
@@ -1,6 +1,7 @@
 package worldofzuul.item;
 
 import worldofzuul.Sprite;
+import java.util.HashMap;
 
 public abstract class Item {
     private String name;
@@ -12,12 +13,22 @@ public abstract class Item {
     public Item(String name) {
         this.name = name;
     }
-    public Item(String name, Double value){ this.name = name; this.value = value; }
-    public Item(String name, Double Value, Double sellbackRate){ this.name = name; this.value = value; this.sellbackRate = sellbackRate; }
+    public Item(String name, Double value){
+        this.name = name; this.value = value;
+    }
+    public Item(String name, Double value, Double sellbackRate){
+        this.name = name;
+        this.value = value;
+        this.sellbackRate = sellbackRate;
+    }
 
     public String getName() {
         return name;
     }
-    public Double getValue() { return value; }
-    public Double getSellbackRate() { return sellbackRate; }
+    public Double getValue() {
+        return value;
+    }
+    public Double getSellbackRate() {
+        return sellbackRate;
+    }
 }
