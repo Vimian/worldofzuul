640e51261d87fb39b273d642021a53ff3be7cd32
Sune <suneroed@gmail.com>
2020-11-17 16:01:36 +0100

    Rettelse på navn til metoderne metoderne

--- src/main/java/worldofzuul/MarketInventory.java
+++ src/main/java/worldofzuul/MarketInventory.java
@@ -6,8 +6,8 @@ import java.util.Collection;
 public class MarketInventory {
     private ArrayList<MarketInventory> stock = new ArrayList<>();
 
-    public void removeItemsFromStock(MarketInventory item) { this.stock.remove(item); }
-    public void addItemsToStock(MarketInventory item) {  this.stock.add(item); }
+    public void removeItemFromStock(MarketInventory item) { this.stock.remove(item); }
+    public void addItemToStock(MarketInventory item) {  this.stock.add(item); }
 
     public ArrayList<MarketInventory> getStock(MarketInventory item) { return stock; }
 }
