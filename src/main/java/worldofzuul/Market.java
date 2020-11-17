00e7e3970cb1c40909f1c2f08c98f1c397b947fc
Sune <suneroed@gmail.com>
2020-11-17 16:02:15 +0100

    Market indtil videre

--- src/main/java/worldofzuul/Market.java
+++ src/main/java/worldofzuul/Market.java
@@ -1,27 +1,38 @@
 package worldofzuul;
 
 import worldofzuul.item.Item;
-
+import worldofzuul.MarketInventory;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Market {
-   public HashMap<Item, Double> stock;
-   public ArrayList<Item> marketplace;
+   public HashMap<Item, Double> stock2;
+   public ArrayList<MarketInventory> stock;
    public Item price;
    public boolean Bought = false;
    public boolean Sold = false;
 
-   public Market(HashMap<Item, Double> stock, ArrayList<Item> marketplace, Item price){
+   public Market(ArrayList<MarketInventory> stock, Item price){
        this.stock = stock;
-       this.marketplace = marketplace;
        this.price = price;
    }
-    public Double purchase(Item item){
-       return stock.remove(item);
+    public Double purchaseItem(MarketInventory item){
+       if(Bought){
+           MarketInventory MI = new MarketInventory();
+           MI.removeItemFromStock(item);
+           Inventory I = new Inventory();
+           I.addItem(item);
+       }
+       return null;
    }
-   public Double sell(Item item){
-       return stock.put(item);
+   public Double sellItem(MarketInventory item){
+       if(Sold){
+           Inventory I = new Inventory();
+           I.removeItem(item);
+           MarketInventory MI = new MarketInventory()
+           MI.addItemToStock(item);
+       }
+           return null;
    }
    public HashMap getItems(Item item){
        return stock.get(item);
