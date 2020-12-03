0ee04f6051fd39c65dbda03dc472522a5eed7f6e
Hampus <16954508+xdHampus@users.noreply.github.com>
2020-12-03 12:10:43 +0100

    Rettelse så GUI kan starte

--- src/main/java/sdu/student/MainApp.java
+++ src/main/java/sdu/student/MainApp.java
@@ -22,7 +22,7 @@ public class MainApp extends Application {
     }
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
         //Game game = new Game();
         //game.play();
     }
