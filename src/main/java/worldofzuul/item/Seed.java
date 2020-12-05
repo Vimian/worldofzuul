914ea3fce00d6a6f1bca6f2a544afe2aa9f3cc88
Sune <suneroed@gmail.com>
2020-12-05 12:41:04 +0100

    Made a randomizer for seedCount

--- src/main/java/worldofzuul/item/Seed.java
+++ src/main/java/worldofzuul/item/Seed.java
@@ -1,28 +1,49 @@
 package worldofzuul.item;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import worldofzuul.Game;
+import worldofzuul.Player;
+import worldofzuul.Inventory;
+import java.util.concurrent.ThreadLocalRandom;
+import worldofzuul.item.SeedType.*;
+import worldofzuul.item.crops.*;
 
 public class Seed extends Item {
     private static final String nameDelimiter = " ";
     private int seedCount;
+    private int maxSeedCount = 20;
+    private int minSeedCount = 0;
 
-
-    public Seed(){
-
-    }
-
-    public Seed(String name, int seedCount) {
-        super(name);
+    public Seed(String name, int seedCount, Double value, Double sellbackRate) {
+        super(name,value,sellbackRate);
         this.seedCount = seedCount;
     }
 
     @JsonIgnore
     public Plant getPlant(){
         seedCount--;
-        return new Plant(super.getName());
+        return new Plant(super.getName(),super.getValue(),super.getSellbackRate());
     }
     public int getSeedCount() {
         return seedCount;
+    }
+
+    public void getSeedsFromPlant(Plant plant, Player player){
+
+        if (player.getInventory().doesContain(plant)) {
+            player.getInventory().removeItem(plant);
+            ThreadLocalRandom randomSeedGenerator = ThreadLocalRandom.current();
+            int randomSeed = randomSeedGenerator.nextInt(minSeedCount, maxSeedCount);
+            seedCount = seedCount + randomSeed;
+            Item Seed = new Seed("Seed",seedCount,2.0,0.20);
+            player.getInventory().addItem(Seed);
+        }
+        else {
+            System.out.println("This is not a plant and does not contain any seeds");
+
+        }
+
+
     }
 
     @Override
