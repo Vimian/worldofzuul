a8bbf44478832e2247bbcabe81bdce800a36e6e9
Hampus <s>
2020-12-09 13:49:47 +0100

    Tilføj map + tsx

---
+++ src/main/resources/sdu/student/sprites/sereneVillage/Serene_Village_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Serene_Village_32x32" tilewidth="32" tileheight="32" tilecount="855" columns="19">
+ <image source="Serene_Village_32x32.png" width="608" height="1440"/>
+</tileset>
